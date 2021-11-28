f1f426261157566a412ccc8eeffab1ec345dd048
Ccoder02 <75584178+Ccoder02@users.noreply.github.com>
2021-11-27 18:58:02 -0800

    Create degree.h

---
+++ degree.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+
+//Enumerated data type declaration
+enum DegreeProgram { SECURITY, NETWORK, SOFTWARE};
+
+static const std::string degreeProgramStrings[] =
+{ "SECURITY", "NETWORK", "SOFTWARE" };
+
